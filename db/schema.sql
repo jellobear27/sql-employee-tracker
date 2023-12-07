6bfe1d1cd2f6c85e83b77f02a2dcf43d7fb7f676
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-12-06 19:34:24 -0500

    added querry.sql, schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+USE employeeTracker_db;
+
+CREATE TABLE departments (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE roles (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255),
+salary DECIMAL(10,2),
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES departments(id)
+ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+FOREIGN KEY (role_id)
+REFERENCES roles(id)
+ON DELETE SET NULL,
+manager_id INT,
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
+ON DELETE SET NULL
+);
